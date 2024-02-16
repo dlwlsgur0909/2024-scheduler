9b45dc0f756efc63a931c3657ff3442f43737a97
LJH09 <dlwlsgur0909@naver.com>
2024-02-16 22:51:40 +0900

    feat/fe: MemberJoin, MemberLogin Page

---
+++ frontend/schduler/src/views/LoginView.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from 'vue';
+
+const memberEmail = ref('');
+const memberPassword = ref('');
+
+
+function login() {
+
+    const loginData = {
+        memberEmail,
+        memberPassword
+    };
+
+    axios.post('http://localhost:8080/members/login', loginData)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="login-form-container">
+            <div class="input-item">
+                <span>이메일</span>
+                <input type="text" v-model="memberEmail">
+            </div>
+            <div class="input-item">
+                <span>비밀번호</span>
+                <input type="text" v-model="memberPassword">
+            </div>
+      </div>
+      
+      <div class="footer">
+                <button type="button" @click="registerMember()">
+                    로그인
+                </button>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+
+.login-form-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.input-item {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.footer {
+  display: flex;
+  justify-content: flex-end;
+}
+
+
+</style>
---
+++ frontend/schduler/src/views/MemberJoinView.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const memberName = ref('');
+const memberEmail = ref('');
+const memberPassword = ref('');
+const memberNickname = ref('');
+
+
+function registerMember() {
+
+  const registerData = {
+    memberName: memberName.value,
+    memberEmail: memberEmail.value,
+    memberPassword: memberPassword.value,
+    memberNickname: memberNickname.value
+  };
+
+  axios.post(`http://localhost:8080/members`, registerData)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+}
+
+
+
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="register-form-container">
+            <div class="input-item">
+                <span>이름</span>
+                <input type="text" v-model="memberName">
+            </div>
+            <div class="input-item">
+                <span>이메일</span>
+                <input type="text" v-model="memberEmail">
+            </div>
+            <div class="input-item">
+                <span>비밀번호</span>
+                <input type="text" v-model="memberPassword">
+            </div>
+            <div class="input-item">
+                <span>닉네임</span>
+                <input type="text" v-model="memberNickname">
+            </div>
+      </div>
+      
+      <div class="footer">
+                <button type="button" @click="registerMember()">
+                    회원가입
+                </button>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+
+.register-form-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.input-item {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.footer {
+  display: flex;
+  justify-content: flex-end;
+}
+
+
+</style>
